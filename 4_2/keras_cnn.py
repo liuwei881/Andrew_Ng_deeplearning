2a0e38fe8fa89b0e70ef8a7009262d2ffce1551e
liuweia <liuweia@mail.open.com.cn>
2019-01-02 14:00:31 +0800

    使用keras搭建简单的cnn
    使用keras搭建简单的cnn

--- 4_2/keras_cnn.py
+++ 4_2/keras_cnn.py
@@ -17,3 +17,89 @@ import keras.backend as K
 K.set_image_data_format('channels_last')
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import imshow
+
+
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+X_train = X_train_orig / 255
+X_test = X_test_orig / 255
+
+Y_train = Y_train_orig.T
+Y_test = Y_test_orig.T
+
+# print("number of training examples = " + str(X_train.shape[0]))
+# print("number of test examples = " + str(X_test.shape[0]))
+# print("X_train shape: " + str(X_train.shape))
+# print("Y_train shape: " + str(Y_train.shape))
+# print("X_test shape: " + str(X_test.shape))
+# print("Y_test shape: " + str(Y_test.shape))
+
+
+def model(input_shape):
+    """
+    test model
+    :param input_shape:
+    :return:
+    """
+    # Define the input placeholder as a tensor with shape input_shape. Think of this as your input image!
+    X_input = Input(input_shape)
+    # Zero-Padding: pads the border of X_input with zeroes
+    X = ZeroPadding2D((3, 3))(X_input)
+    # CONV -> BN -> RELU Block applied to X
+    X = Conv2D(32, (7, 7), strides=(1, 1), name='conv0')(X)
+    X = BatchNormalization(axis=3, name='bn0')(X)
+    X = Activation('relu')(X)
+
+    # MAXPOOL
+    X = MaxPooling2D((2, 2), name='max_pool')(X)
+    # FLATTEN X (means convert it to a vector) + FULLYCONNECTED
+    X = Flatten()(X)
+    X = Dense(1, activation='sigmoid', name='fc')(X)
+    # Create model. This creates your Keras model instance, you'll use this instance to train/test the model.
+    model = Model(inputs=X_input, outputs=X, name='HappyModel')
+    return model
+
+
+def HappyModel(input_shape):
+    """
+    Implementation of the HappyModel.
+    Arguments:
+        input_shape -- shape of the images of the dataset
+    Returns:
+        model -- a Model() instance in Keras
+    """
+    X_input = Input(input_shape)
+    X = ZeroPadding2D((3, 3))(X_input)
+    X = Conv2D(32, (7, 7), strides=(1, 1), name='conv0')(X)
+    X = BatchNormalization(axis=3, name='bn0')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((2, 2), name='max_pool')(X)
+    X = Flatten()(X)
+    X = Dense(1, activation='sigmoid', name='fc')(X)
+    model = Model(inputs=X_input, outputs=X, name='HappyModel')
+    return model
+
+
+happyModel = HappyModel(X_train.shape[1:])
+happyModel.compile(optimizer="Adam", loss="binary_crossentropy", metrics=["accuracy"])
+happyModel.fit(x=X_train, y=Y_train, epochs=20, batch_size=32)
+# 评估模型
+preds = happyModel.evaluate(X_test, Y_test)
+print("Loss = " + str(preds[0]))
+print("Test Accuracy = " + str(preds[1]))
+
+# - You can use MAXPOOL after such blocks. It will help you lower the dimension in height and width.
+# 可以使用最大池化, 来降低高度和宽度
+# - Change your optimizer. We find Adam works well.
+# 变换你的优化器, Adam表现的很好
+# - If the model is struggling to run and you get memory issues, lower your batch_size (12 is usually a good compromise)
+# 内存不够, 就是用妥协的batch_size=12.
+# - Run on more epochs, until you see the train accuracy plateauing.
+# 多增加epochs, 观察train精度是否有收敛.
+# happyModel.summary()
+# plot_model(happyModel, to_file='HappyModel.png')
+# SVG(model_to_dot(happyModel).create(prog='dot', format='svg'))
+
+
+
+
+
